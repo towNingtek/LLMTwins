c323f8cded22bedc88302b53ef224eb333c972ec
yillkid <yillkid@gmail.com>
2026-01-05 23:20:56 +0800

    chore: 改用 docker compose 啟動開發環境

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
-source env/bin/activate
-uvicorn server:app --host 0.0.0.0 --port 7999 --reload
+# source env/bin/activate
+# uvicorn server:app --host 0.0.0.0 --port 7999 --reload
+
+docker compose -f docker-compose.dev.yml down
+docker compose -f docker-compose.dev.yml up
